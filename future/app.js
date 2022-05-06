01ad12036a10e8397f0f570f86f4906ac555616b
Muhammadsalim20 <92430075+Muhammadsalim20@users.noreply.github.com>
2022-05-06 19:53:21 +0500

    Add files via upload

---
+++ future/app.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLikns = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLikns.classList.toggle('active');
+    })
+let app = {
+    name =
+}
